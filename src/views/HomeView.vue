3d4d98311ebb34df3b0399c9a5b888ba2a795fb3
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-08 17:26:47 +0300

    Created static layout for Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,18 @@
   <div class="home">
     <NavbarBootstrap/>
     <ContainerBootstrap>
+      <div class="row justify-content-center">
+        <div class="col-12 d-flex justify-content-center">
+          <img class="avatar shadow-lg" alt="avatar" src="@/assets/images/avatar.png">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 text-center">
+          <h2>Тестовое приложение "Туду лист"</h2>
+          <p>Обучающее приложение 💼</p>
+          <a href="#">К задачам</a>
+        </div>
+      </div>
     </ContainerBootstrap>
   </div>
 </template>
@@ -15,3 +27,15 @@ export default {
   components: {NavbarBootstrap, ContainerBootstrap}
 }
 </script>
+<style scoped lang="scss">
+.avatar {
+  margin-top: 2rem;
+  border-radius: 50%;
+}
+h2 {
+  margin-top: 1rem;
+}
+p {
+  margin-top: 1rem;
+}
+</style>
