5cd728aaeacb4ffbaaf81e460b79cdb89e64ebaa
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-09 14:41:40 +0300

    created TodoItem.vue

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Приготовить покушать</h5>
+        <p class="card-text">Очень срочное задание</p>
+        <div class="d-flex align-content-center justify-content-between">
+          <div class="btn-list">
+            <button type="button" class="btn btn-primary">Изменить</button>
+            <button type="button" class="btn btn-danger">Удалить</button>
+          </div>
+          <div class="card-date d-inline-flex text-muted">
+            <span class="align-self-center">09.08.2022 14:28</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem"
+}
+</script>
+
+<style lang="scss" scoped>
+.btn-list {
+  button:first-child {
+    margin-right: 1rem;
+  }
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,38 +1,19 @@
 <template>
 <div class="row row-cols-1 g-4">
-  <div class="col">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Приготовить покушать</h5>
-        <p class="card-text">Очень срочное задание</p>
-        <div class="d-flex align-content-center justify-content-between">
-          <div class="btn-list">
-            <button type="button" class="btn btn-primary">Изменить</button>
-            <button type="button" class="btn btn-danger">Удалить</button>
-          </div>
-          <div class="card-date d-inline-flex text-muted">
-            <span class="align-self-center">09.08.2022 14:28</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <TodoItem/>
 </div>
 </template>
 
 <script>
+import TodoItem from "@/components/TodoItem";
 export default {
-  name: "TodoList"
+  name: "TodoList",
+  components: {TodoItem}
 }
 </script>
 
 <style lang="scss" scoped>
 .row {
   margin-top: 2rem;
-}
-.btn-list {
-  button:first-child {
-    margin-right: 1rem;
-  }
 }
 </style>
