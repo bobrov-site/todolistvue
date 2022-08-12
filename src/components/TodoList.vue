e736f6c7f471cd517b97e4e3b13c05892f491521
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-12 13:58:39 +0300

    added animation for item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="row row-cols-1 g-4">
-  <TodoItem v-for="todo in todos" :todo="todo" :key="todo.id"/>
-</div>
+  <transition-group name="todos-list" tag="div" class="row row-cols-1 g-4">
+    <TodoItem v-for="todo in todos" :todo="todo" :key="todo.id"/>
+  </transition-group>
 </template>
 
 <script>
@@ -21,5 +21,21 @@ export default {
 <style lang="scss" scoped>
 .row {
   margin-top: 2rem;
+}
+.todos-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.todos-list-enter-active,
+.todos-list-leave-active {
+  transition: all 0.4s ease;
+}
+.todos-list-enter-from,
+.todos-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.todos-list-move {
+  transition: transform 0.4s ease;
 }
 </style>
