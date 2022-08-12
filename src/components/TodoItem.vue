a8d880cd254c01f00a091d9d7e29fade6fe6506e
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-12 13:48:41 +0300

    call modal for changing todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -12,8 +12,9 @@
         <p v-else class="card-text text-danger">Невыполнено</p>
         <div class="d-flex align-content-center justify-content-between">
           <div class="btn-list">
-            <button-bootstrap css-class="btn-primary">Изменить</button-bootstrap>
+            <button-bootstrap data-bs-toggle="modal" data-bs-target="#changeTodo" css-class="btn-primary">Изменить</button-bootstrap>
             <button-bootstrap @click.native="removeTodo(todo.id)" css-class="btn-danger">Удалить</button-bootstrap>
+            <ModalBootstrap :todo="todo" css-id="changeTodo"/>
           </div>
           <div class="card-date d-inline-flex text-muted">
             <span class="align-self-center">id {{todo.id}}</span>
@@ -26,10 +27,11 @@
 
 <script>
 import ButtonBootstrap from "@/components/UI/ButtonBootstrap";
+import ModalBootstrap from "@/components/UI/ModalBootstrap";
 
 export default {
   name: "TodoItem",
-  components: {ButtonBootstrap},
+  components: {ModalBootstrap, ButtonBootstrap},
   props: {
     todo: {
       type: Object,
