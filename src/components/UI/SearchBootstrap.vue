65b9cb4ead69ca7e599cfb4b85b7cc262813e2b3
Daniil Bobrov <bobdaniil@yandex.ru>
2022-09-09 11:08:50 +0300

    removed submit search button

--- src/components/UI/SearchBootstrap.vue
+++ src/components/UI/SearchBootstrap.vue
@@ -1,23 +1,15 @@
 <template>
   <form class="d-flex">
     <input class="form-control me-2" type="text" placeholder="Найти задачу" v-model="$store.state.todos.searchQuery" aria-label="Search">
-    <button-bootstrap @click.native="searchTodo" css-class="btn-outline-success">Поиск</button-bootstrap>
   </form>
 </template>
 
 <script>
-import buttonBootstrap from "@/components/UI/ButtonBootstrap";
 export default {
   name: "SearchBootstrap",
-  components: {buttonBootstrap},
   data() {
     return {
       title: '',
-    }
-  },
-  methods: {
-    searchTodo() {
-      this.$emit('search', this.title);
     }
   }
 }
