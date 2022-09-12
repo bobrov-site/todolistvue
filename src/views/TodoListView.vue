4d086683932afb4b0ea1a696b271432e47e27b8e
Daniil Bobrov <bobdaniil@yandex.ru>
2022-09-12 12:33:15 +0300

    create PaginationBootstrap.vue and added to TodoListView.vue

--- src/views/TodoListView.vue
+++ src/views/TodoListView.vue
@@ -14,6 +14,7 @@
       </div>
     </div>
     <TodoList v-if="todos" :todos="searchedTodos"/>
+    <PaginationBootstrap/>
   </ContainerBootstrap>
 </div>
 </template>
@@ -26,9 +27,12 @@ import {mapState, mapActions, mapMutations, mapGetters} from 'vuex'
 import ButtonBootstrap from "@/components/UI/ButtonBootstrap";
 import ModalBootstrap from "@/components/UI/ModalBootstrap";
 import SearchBootstrap from "@/components/UI/SearchBootstrap";
+import PaginationBootstrap from "@/components/UI/PaginationBootstrap";
 export default {
   name: "TodoListView",
-  components: {SearchBootstrap, ModalBootstrap, TodoList , ButtonBootstrap, TitlePage, ContainerBootstrap},
+  components: {
+    PaginationBootstrap,
+    SearchBootstrap, ModalBootstrap, TodoList , ButtonBootstrap, TitlePage, ContainerBootstrap},
   data: function() {
     return {
       isShow: false,
