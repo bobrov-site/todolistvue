0e4052d208e6eb8c616cd81e2aeee724aebd6270
Daniil Bobrov <bobdaniil@yandex.ru>
2022-09-28 16:47:31 +0300

    created disabled button for previous and next pagination

--- src/components/UI/PaginationBootstrap.vue
+++ src/components/UI/PaginationBootstrap.vue
@@ -1,11 +1,15 @@
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li class="page-item"><a class="page-link" href="#">Предыдущая</a></li>
+      <li :class="{disabled : previousDisable}" class="page-item">
+        <a @click="previousPage(page)" class="page-link" href="#">Предыдущая</a>
+      </li>
       <li v-for="pageNumber in totalPages" :key="pageNumber" :class="{'active' : page === pageNumber}" class="page-item">
         <span @click="changePage(pageNumber)" class="page-link">{{pageNumber}}</span>
       </li>
-      <li class="page-item"><a class="page-link" href="#">Далее</a></li>
+      <li :class="{disabled : nextDisable}" class="page-item">
+        <a class="page-link" href="#">Далее</a>
+      </li>
     </ul>
   </nav>
 </template>
@@ -13,6 +17,12 @@
 <script>
 export default {
   name: "PaginationBootstrap",
+  data() {
+    return {
+      previousDisable: false,
+      nextDisable: false,
+    }
+  },
   props: {
     page: Number,
     totalPages: Number
@@ -27,6 +37,16 @@ export default {
         this.$router.push({name: 'todolistPagination', params: {page: pageNumber}})
       }
     }
+  },
+  watch: {
+    page: function (val) {
+      console.log(val)
+      this.nextDisable = val === this.totalPages;
+    }
+  },
+  created() {
+    this.previousDisable = this.page === 1;
+    this.nextDisable = this.page === this.totalPages;
   }
 }
 </script>
