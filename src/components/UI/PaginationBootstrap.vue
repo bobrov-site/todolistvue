93d8a3c345b535df5f180fca16c486b6cf2d3de5
Daniil Bobrov <bobdaniil@yandex.ru>
2022-09-29 17:59:51 +0300

    added logic for disabling the new and prev page for PaginationBootstrap.vue

--- src/components/UI/PaginationBootstrap.vue
+++ src/components/UI/PaginationBootstrap.vue
@@ -1,14 +1,14 @@
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li :class="{disabled : previousDisable}" class="page-item">
-        <a @click="previousPage(page)" class="page-link" href="#">Предыдущая</a>
+      <li @click="previousPage(page)" :class="{disabled : previousDisable}" class="page-item">
+        <router-link :to="'/todolist/' + String(page - 1)" class="page-link">Предыдущая</router-link>
       </li>
       <li v-for="pageNumber in totalPages" :key="pageNumber" :class="{'active' : page === pageNumber}" class="page-item">
         <span @click="changePage(pageNumber)" class="page-link">{{pageNumber}}</span>
       </li>
-      <li :class="{disabled : nextDisable}" class="page-item">
-        <a class="page-link" href="#">Далее</a>
+      <li @click="nextPage(page)" :class="{disabled : nextDisable}" class="page-item">
+        <router-link :to="'/todolist/' + String(page + 1)" class="page-link">Далее</router-link>
       </li>
     </ul>
   </nav>
@@ -36,17 +36,26 @@ export default {
       else {
         this.$router.push({name: 'todolistPagination', params: {page: pageNumber}})
       }
+    },
+    nextPage(pageNumber) {
+      const nextPageNumber = Number(pageNumber + 1)
+      this.$store.commit('todos/setPage', nextPageNumber)
+    },
+    previousPage(pageNumber) {
+      const previousPageNumber = Number(pageNumber - 1);
+      this.$store.commit('todos/setPage', previousPageNumber);
     }
+  },
+  mounted() {
+    this.nextDisable = this.page === this.totalPages;
+    this.previousDisable = this.page === 1;
   },
   watch: {
     page: function (val) {
       console.log(val)
       this.nextDisable = val === this.totalPages;
+      this.previousDisable = val === 1;
     }
-  },
-  created() {
-    this.previousDisable = this.page === 1;
-    this.nextDisable = this.page === this.totalPages;
   }
 }
 </script>
