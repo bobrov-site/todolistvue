64dcb4be51d782cc5b4db6233cdbccd7de4bd42d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-09-30 14:51:16 +0300

    created BadgeBootstrap.vue

---
+++ src/components/UI/BadgeBootstrap.vue
@@ -0,0 +1,23 @@
+<template>
+<span :class="cssClass" class="badge">{{text}}</span>
+</template>
+
+<script>
+export default {
+  name: "BadgeBootstrap",
+  props: {
+    cssClass: {
+      type: String,
+      require: true,
+    },
+    text: {
+      type: String,
+      require: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
