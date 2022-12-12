4ce9f85efdd1327f1e2abec4cb09ec6c8645cb0f
Daniil Bobrov <bobdaniil@yandex.ru>
2022-12-12 15:35:56 +0300

    installed firebase

--- src/main.js
+++ src/main.js
@@ -8,6 +8,26 @@ import {ValidationObserver} from "vee-validate";
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.component('ValidationObserver', ValidationObserver)
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDeTgmPw34ZhqsL4Tfv5zsFykek_A_WcFw",
+  authDomain: "todolistvue-5ea9a.firebaseapp.com",
+  projectId: "todolistvue-5ea9a",
+  storageBucket: "todolistvue-5ea9a.appspot.com",
+  messagingSenderId: "530284892850",
+  appId: "1:530284892850:web:63567dba67910caf812fab",
+  measurementId: "G-7821MBSJRW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 Vue.config.productionTip = false
 
 new Vue({
