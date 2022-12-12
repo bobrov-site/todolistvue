a052675925292a8ba8af8db03ffbe7594d46a38d
Daniil Bobrov <bobdaniil@yandex.ru>
2022-12-12 17:23:30 +0300

    created SignInView.vue

---
+++ src/views/SignInView.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="signIn">
+  <ContainerBootstrap>
+
+  </ContainerBootstrap>
+</div>
+</template>
+
+<script>
+import ContainerBootstrap from "@/components/UI/ContainerBootstrap.vue";
+
+export default {
+  name: "SignInView",
+  components: {ContainerBootstrap}
+}
+</script>
+
+<style scoped>
+
+</style>
