47a8f9a9f1a12df56dbd742486d440edf8d43339
Daniil Bobrov <bobdaniil@yandex.ru>
2022-12-21 11:30:38 +0300

    added user log in when page reloaded.

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,22 @@
 <script>
 import NavbarBootstrap from "@/components/UI/NavbarBootstrap";
 import FooterBootstrap from "@/components/UI/FooterBootstrap";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+
 export default {
   components: {NavbarBootstrap, FooterBootstrap},
+  mounted() {
+    const auth = getAuth()
+    auth.currentUser;
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.$store.commit('user/logInUser', [user.email, user.displayName, user.uid, user.photoURL, true])
+      }
+      else {
+        console.log('пользователь не в системе')
+      }
+    })
+  }
 }
 </script>
 <style lang="scss">
